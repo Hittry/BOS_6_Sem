bfd693704d3ff1dc6273a913f87955f5ad842bd4
Alexander Shuvalov <Alexander.Shuvalov@infowatch.com>
2021-05-28 16:08:04 +0300

    Lab_proc

---
+++ proc/c_2_2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{
+  int pid = fork();
+  // определить, в каком процессе мы находимся, помогает переменная pid
+
+  if (pid == 0) {
+    printf("Children pid: %d\n", getpid());
+    printf("Parent pid: %d\n", getppid());
+    printf("Это сообщение из дочернего процесса\n");
+	sleep(3);
+  } else if (pid > 0) {
+    // родительский процесс получает значение PID дочернего, он должен быть > 0
+	sleep(3);
+    printf("Это сообщение из родительского процесса.\n"
+           "Идентификатор дочернего процесса:  %d\n", pid);
+  }
+
+  return 0;
+}
